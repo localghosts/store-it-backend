bf7830fb1dec0e62a0bf2d1c0429a8dba50e01d7
Bhuvan Singla <bhuvansingla2000@gmail.com>
2022-01-24 01:05:05 +0530

    Initalise Spring Boot project

---
+++ src/main/java/com/localghosts/storeit/StoreItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localghosts.storeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreItBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localghosts/storeit/StoreItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localghosts.storeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreItBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
