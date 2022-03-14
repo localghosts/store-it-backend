895cd73c0b14563d48da5eda18fdade353986637
Harshit Raj <harshitr20@iitk.ac.in>
2022-03-15 02:13:34 +0530

    Add Cart endpoint

--- src/main/java/com/localghosts/storeit/controller/StoreController.java
+++ src/main/java/com/localghosts/storeit/controller/StoreController.java
@@ -2,14 +2,23 @@ package com.localghosts.storeit.controller;
 
 import java.util.List;
 
+import com.localghosts.storeit.config.BuyerRepo;
+import com.localghosts.storeit.config.CartRepo;
 import com.localghosts.storeit.config.CategoryRepo;
+import com.localghosts.storeit.config.OrderRepo;
 import com.localghosts.storeit.config.ProductRepo;
 import com.localghosts.storeit.config.StoreRepo;
+import com.localghosts.storeit.model.Buyer;
+import com.localghosts.storeit.model.Cart;
+import com.localghosts.storeit.model.CartResponse;
 import com.localghosts.storeit.model.Category;
+import com.localghosts.storeit.model.Order;
 import com.localghosts.storeit.model.Product;
 import com.localghosts.storeit.model.Store;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +37,15 @@ public class StoreController {
 
 	@Autowired
 	CategoryRepo categoryRepo;
+
+	@Autowired
+	CartRepo cartRepo;
+
+	@Autowired
+	BuyerRepo buyerRepo;
+
+	@Autowired
+	OrderRepo orderRepo;
 
 	@GetMapping("/store")
 	public List<Store> getStores() {
@@ -130,4 +148,64 @@ public class StoreController {
 		return lowercaseaplhaString;
 	}
 
+	@PostMapping("/store/{storeslug}/cart/{productid}/{quantity}")
+	public String addToCart(@PathVariable("storeslug") String storeslug, @PathVariable("productid") long productid,
+			@PathVariable("quantity") int quantity, Authentication auth) {
+
+		if (quantity < 0)
+			throw new Error("Invalid quantity");
+
+		Product product = productRepo.findByProductID(productid);
+		Buyer buyer = buyerRepo.findByEmail(auth.getName());
+		Store store = storeRepo.findByStoreslug(storeslug);
+
+		if (product == null || buyer == null || store == null)
+			throw new Error("Invalid request");
+
+		Cart cart = cartRepo.findTop1ByBuyerAndStoreAndProduct(buyer, store, product);
+
+		if (cart == null) {
+			if (quantity == 0)
+				throw new Error("Invalid quantity");
+			cartRepo.save(new Cart(buyer, store, product, quantity));
+		}
+
+		if (quantity == 0)
+			cartRepo.delete(cart);
+
+		cart.setQuantity(quantity);
+		cartRepo.save(cart);
+
+		return "Product added to cart";
+	}
+
+	@GetMapping("/store/{storeslug}/cart/")
+	public CartResponse getCart(@PathVariable("storeslug") String storeslug, Authentication auth) {
+		Buyer buyer = buyerRepo.findByEmail(auth.getName());
+		Store store = storeRepo.findByStoreslug(storeslug);
+		if (buyer == null || store == null)
+			throw new Error("Invalid request");
+		List<Cart> cartlist = cartRepo.findByBuyerAndStore(buyer, store);
+		Long total = cartlist.stream().mapToLong(Cart::getPrice).sum();
+		CartResponse cartResponse = new CartResponse(total, cartlist);
+		return cartResponse;
+	}
+
+	@PostMapping("/store/{storeslug}/checkout")
+	public String checkout(@PathVariable("storeslug") String storeslug, Authentication auth) {
+		Buyer buyer = buyerRepo.findByEmail(auth.getName());
+		Store store = storeRepo.findByStoreslug(storeslug);
+
+		if (buyer == null || store == null)
+			throw new Error("Invalid request");
+
+		List<Cart> cartlist = cartRepo.findByBuyerAndStore(buyer, store);
+
+		for (Cart cart : cartlist) {
+			orderRepo.save(new Order(cart));
+			cartRepo.delete(cart);
+		}
+
+		return "Order placed successfully";
+	}
 }
--- src/main/java/com/localghosts/storeit/model/Cart.java
+++ src/main/java/com/localghosts/storeit/model/Cart.java
@@ -13,28 +13,39 @@ import javax.persistence.Table;
 @Table(name = "cart")
 public class Cart {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long cartID;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cartID;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "buyerID", nullable = false)
-	private Buyer buyer;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyerID", nullable = false)
+    private Buyer buyer;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "itemID", nullable = false)
-	private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "StoreID", nullable = false)
+    private Store store;
 
-	public Cart() {
-	}
+    public Cart(Buyer buyer, Store store, Product product, int quantity) {
+        this.buyer = buyer;
+        this.store = store;
+        this.product = product;
+        this.quantity = quantity;
+    }
 
-	public Cart(Buyer buyer, Product product, int quantity) {
-		this.buyer = buyer;
-		this.product = product;
-		this.quantity = quantity;
-	}
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productID", nullable = false)
+    private Product product;
 
-	private int quantity;
+    private int quantity;
+
+    public Cart() {
+    }
+
+    public Cart(Buyer buyer, Product product, int quantity) {
+        this.buyer = buyer;
+        this.product = product;
+        this.quantity = quantity;
+    }
 
     /**
      * @return Long return the cartID
@@ -92,4 +103,21 @@ public class Cart {
         this.quantity = quantity;
     }
 
+    /**
+     * @return Store return the store
+     */
+    public Store getStore() {
+        return store;
+    }
+
+    /**
+     * @param store the store to set
+     */
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public int getPrice() {
+        return product.getPrice() * quantity;
+    }
 }
