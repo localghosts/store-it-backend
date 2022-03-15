9071c9d1fd6a7ea30e3874148433e7d042d255fb
Harshit Raj <harshitr20@iitk.ac.in>
2022-03-15 06:08:59 +0530

    Add product endpoints

--- src/main/java/com/localghosts/storeit/config/ProductRepo.java
+++ src/main/java/com/localghosts/storeit/config/ProductRepo.java
@@ -1,9 +1,14 @@
 package com.localghosts.storeit.config;
 
+import java.util.List;
+
+import com.localghosts.storeit.model.Category;
 import com.localghosts.storeit.model.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 	Product findByProductID(long Id);
+
+	List<Product> findByCategory(Category category);
 }
