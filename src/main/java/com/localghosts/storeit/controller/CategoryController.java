3792de1397011a17f0c1beaa104eef9cdc7bc231
Harshit Raj <harshitr20@iitk.ac.in>
2022-03-17 18:28:53 +0530

    add toggle APIs

--- src/main/java/com/localghosts/storeit/controller/CategoryController.java
+++ src/main/java/com/localghosts/storeit/controller/CategoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -55,6 +56,13 @@ public class CategoryController {
 	public void deleteCategory(@PathVariable("categoryid") Long categoryid) {
 		Category category = categoryRepo.findByCategoryID(categoryid);
 		categoryRepo.delete(category);
+	}
+
+	@PutMapping("/store/{storeslug}/category/{categoryid}/toggle")
+	public void toggleCategory(@PathVariable("categoryid") Long categoryid) {
+		Category category = categoryRepo.findByCategoryID(categoryid);
+		category.setEnabled(!category.isEnabled());
+		categoryRepo.save(category);
 	}
 
 }
--- src/main/java/com/localghosts/storeit/controller/ProductController.java
+++ src/main/java/com/localghosts/storeit/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +51,13 @@ public class ProductController {
 	public void deleteProduct(@PathVariable("productid") Long productid) {
 		Product product = productRepo.findByProductID(productid);
 		productRepo.delete(product);
+	}
+
+	@PutMapping("/store/{storeslug}/product/{productid}/toggle")
+	public void toggleProduct(@PathVariable("productid") Long productid) {
+		Product product = productRepo.findByProductID(productid);
+		product.setInstock(!product.isInstock());
+		productRepo.save(product);
 	}
 
 }
