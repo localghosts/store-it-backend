c2cc8168409b790b8c3e1363a8c49b20b109c66e
antreev-brar <antreev.brar@gmail.com>
2022-04-03 19:41:35 +0530

    more test

--- src/test/java/com/localghosts/storeit/BuyerOrders.java
+++ src/test/java/com/localghosts/storeit/BuyerOrders.java
@@ -9,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,13 +26,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import com.localghosts.storeit.controller.BuyerAuthController;
-import com.localghosts.storeit.controller.SellerAuthController;
+
+import com.localghosts.storeit.model.Buyer;
 import com.localghosts.storeit.model.JwtResponse;
-import com.localghosts.storeit.model.Seller;
-import com.localghosts.storeit.repo.SellerRepo;
+
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -39,25 +41,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class SellerLogin{
+public class BuyerOrders{
 	
 	    @Test
-	    public void sellerLogin() throws Exception {
+	    public void buerLoginfunc() throws Exception {
 		    
 		 
 		    TestRestTemplate template = new TestRestTemplate();
-		    final String baseUrl = "http://localhost:8080/seller/login";
+		    final String baseUrl = "http://localhost:8080/buyer/login";
 	        URI uri = new URI(baseUrl);
 	        
-		    Seller seller = new Seller();
+		    Buyer buyer = new Buyer();
 		    
-		    seller.setName("nope");
-		    seller.setEmail("harshit9304@gmail.com");
-		    seller.setPassword("123");
+		  
+		    buyer.setEmail("antdany1298@gmail.com");
+		    buyer.setPassword("123");
 		    
 		    HttpHeaders headers = new HttpHeaders();     
 	        headers.setContentType(MediaType.APPLICATION_JSON);
-	        HttpEntity<Seller> request = new HttpEntity<Seller>(seller, headers);
+	        HttpEntity<Buyer> request = new HttpEntity<Buyer>(buyer, headers);
 	        
 	        
 	        System.out.println(request.getHeaders());
@@ -76,6 +78,33 @@ public class SellerLogin{
 	        System.out.println(result.getHeaders());
 	        System.out.println(result.getBody());
 	        Assert.assertEquals(200, result.getStatusCodeValue());
+	        
+	        /////////////////////////////////////////////////////////////
+	        
+	        
+	        JSONObject myobj = new JSONObject(body);
+	        System.out.println(myobj.getString("token"));
+	        System.out.println(myobj.getString("storeSlug"));
+	        String bearerString = myobj.getString("token") ; 
+	        
+	        //String bearerString = result.getBody().get
+	        ////
+	        TestRestTemplate templatestore = new TestRestTemplate();
+	        final String baseUrlstore = "http://localhost:8080/orders";
+	        URI uristore = new URI(baseUrlstore);
+	        HttpHeaders headerstore = new HttpHeaders();  
+	        headerstore.setContentType(MediaType.APPLICATION_JSON);
+	        headerstore.add("Authorization", bearerString);
+//	        
+//	        
+	        ResponseEntity<String> entity = templatestore.exchange(
+	                uristore, HttpMethod.GET, new HttpEntity<Object>(headerstore),
+	                String.class);
+	        System.out.print(entity.getBody());
+	        Assert.assertEquals(HttpStatus.OK, entity.getStatusCode());
+	        
+	        
+	        
 	    }
 
 }
