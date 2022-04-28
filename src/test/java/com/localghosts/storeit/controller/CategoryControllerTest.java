4d3b88f11cb1131558555137d4d6cae372797e78
Deepankur Kansal <deepank@iitk.ac.in>
2022-04-28 23:09:16 +0530

    Update CategoryControllerTest.java

--- src/test/java/com/localghosts/storeit/controller/CategoryControllerTest.java
+++ src/test/java/com/localghosts/storeit/controller/CategoryControllerTest.java
@@ -1,20 +1,44 @@
 package com.localghosts.storeit.controller;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
+import com.localghosts.storeit.model.Category;
+Import.com.localghosts.storeit.controller.CategoryController;
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.http.ResponseEntity;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 public class CategoryControllerTest {
+@Autowired
+        private CategoryController controller;
+@Utilility
+              public void print_assert(ResponseEntity<String> result ){
+System.out.println(result.getBody());
+System.out.println(result.getStatusCodeValue())System.out.println(result.getHeaders());
+System.out.println(result.getBody());
+Assert.assertEquals(200, result.getStatusCodeValue());
+return;
+}
+@Test
+        public void contextLoads() throws Exception {
+                assertThat(controller).isNotNull();
+                             Category test = new Category(name=“New”, image=“image”, description=“desc”);
+CategoryController auth = new CategoryController();
 
-	@Autowired
-	private CategoryController controller;
+try{
+auth.getCategory(test,”store1”);
+}
+catch (Exception e) { System.out.println(e );}
+//Store added
 
-	@Test
-	public void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
+try{
+auth.deleteCategory(test,”store1”);
+}
+catch (Exception e) { System.out.println(e );}
+//Store deleted
 
-	}
+}
 }
