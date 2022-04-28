2ed0fe29f12b34f500c960675f338b2ca54cb698
Deepankur Kansal <deepank@iitk.ac.in>
2022-04-29 00:00:41 +0530

    Update CategoryControllerTest.java

--- src/test/java/com/localghosts/storeit/controller/CategoryControllerTest.java
+++ src/test/java/com/localghosts/storeit/controller/CategoryControllerTest.java
@@ -1,7 +1,7 @@
 package com.localghosts.storeit.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.localghosts.storeit.model.Category;
-Import.com.localghosts.storeit.controller.CategoryController;
+import.com.localghosts.storeit.controller.CategoryController;
 import org.junit.jupiter.api.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
@@ -15,30 +15,30 @@ public class CategoryControllerTest {
 @Autowired
         private CategoryController controller;
 @Utilility
-              public void print_assert(ResponseEntity<String> result ){
-System.out.println(result.getBody());
-System.out.println(result.getStatusCodeValue())System.out.println(result.getHeaders());
-System.out.println(result.getBody());
-Assert.assertEquals(200, result.getStatusCodeValue());
-return;
-}
+        public void print_assert(ResponseEntity<String> result ){
+                System.out.println(result.getBody());
+                System.out.println(result.getStatusCodeValue())System.out.println(result.getHeaders());
+                System.out.println(result.getBody());
+                Assert.assertEquals(200, result.getStatusCodeValue());
+                return;
+               }
 @Test
         public void contextLoads() throws Exception {
                 assertThat(controller).isNotNull();
-                             Category test = new Category(name=“New”, image=“image”, description=“desc”);
-CategoryController auth = new CategoryController();
+                Category test = new Category(name=“New”, image=“image”, description=“desc”);
+                CategoryController auth = new CategoryController();
 
-try{
-auth.getCategory(test,”store1”);
-}
-catch (Exception e) { System.out.println(e );}
-//Store added
+                try{
+                auth.getCategory(test,”store1”);
+                }
+                catch (Exception e) { System.out.println(e );}
+                //Store added
 
-try{
-auth.deleteCategory(test,”store1”);
-}
-catch (Exception e) { System.out.println(e );}
-//Store deleted
+                try{
+                auth.deleteCategory(test,”store1”);
+                }
+                catch (Exception e) { System.out.println(e );}
+                //Store deleted
 
 }
 }
